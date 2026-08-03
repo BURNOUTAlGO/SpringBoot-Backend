670fb8fccb222dc195bd2c8f6b3afcc95ebb391b
Abhinav Maurya <149594822+BURNOUTAlGO@users.noreply.github.com>
2026-08-03 17:27:36 +0530

    Delete SpringDemo2 directory

--- SpringDemo2/src/main/java/com/bhaiyedependencyhai/CartService.java
+++
@@ -1,9 +0,0 @@
-package com.bhaiyedependencyhai;
-
-public class CartService {
-    public void addToCart(){
-        System.out.println("Added to cart...");
-    }
-}
-
-//bhai ye alag project hai jisko hum dependency ki tarah use karenge SpringIOCContainer project mein.
